62db75902f8194564071cdf6196237601cd211bc
iamharisai <harisai.58@gmail.com>
2025-11-21 15:06:55 +0530

    fix: renames to avoid names with packages

--- 101/openai-sdk.py
+++ 101/openai-sdk.py
@@ -1,9 +1,15 @@
 from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
 client = OpenAI()
+
+
 
 response = client.responses.create(
     model="gpt-5",
-    input="Write a short bedtime story about a unicorn."
+    input="Tell me a joke about programmers in one sentence."
 )
 
 print(response.output_text)
